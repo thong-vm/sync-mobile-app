3906a7c38a3a83d502d5bf4405bcf5686cf9ea58
thong-vm <thongvmsyncpartners@gmail.com>
2024-09-16 10:11:51 +0900

    update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sync Partners App !!!</Text>
+      <Text>Sync Partners Mobile App !!!</Text>
       <StatusBar style="auto" />
     </View>
   );
